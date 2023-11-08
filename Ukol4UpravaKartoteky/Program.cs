21c05b20c82f1b850523ead334c8dee2696ce777
Tynka113 <macha@DESKTOP-TMSSHNL>
2023-11-08 21:08:26 +0100

    základní verze

--- Ukol4UpravaKartoteky/Program.cs
+++ Ukol4UpravaKartoteky/Program.cs
@@ -11,3 +11,4 @@ který bude tvého vlastního typu a v menu mohou být přidány položky
 pro uložení do souboru a načtení ze souboru. 
 Až při této volbě se položky do souboru uloží respektive načtou. 
 */
+
