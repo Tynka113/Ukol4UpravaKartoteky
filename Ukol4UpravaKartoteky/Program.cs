97d0c72b50bf5e00147253fde6c22ee6aeb1574d
Tynka113 <macha@DESKTOP-TMSSHNL>
2023-11-08 20:22:13 +0100

    Add project files.

---
+++ Ukol4UpravaKartoteky/Program.cs
@@ -0,0 +1,13 @@
+﻿
+
+
+/*
+ * Zadání:
+Použižj zadání ze 3. lekce a uprav úkol s kartotékou tak, 
+že bude využívat typ list.
+
+Tedy v průběhu aplikace se budou položky ukládat do listu, 
+který bude tvého vlastního typu a v menu mohou být přidány položky 
+pro uložení do souboru a načtení ze souboru. 
+Až při této volbě se položky do souboru uloží respektive načtou. 
+*/
